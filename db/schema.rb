f70d227823e879356df35903ea79a8a9200bda26
Esmé Cowles <escowles@ticklefish.org>
2022-06-22 11:16:51 -0400

    generating user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_17_214306) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_22_151424) do
   create_table "collections", force: :cascade do |t|
     t.string "manifest_url"
     t.string "html_url"
@@ -37,6 +37,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_17_214306) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["collection_id"], name: "index_manifests_on_collection_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "images", "manifests"
