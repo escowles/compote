9aa94d2b2241bac92d98bfe9e41e79e47a206167
Esmé Cowles <escowles@ticklefish.org>
2022-06-23 12:51:11 -0400

    adding additional config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "iiif-compote.herokuapp.com" }
+  Rails.application.routes.default_url_options[:host] = 'iiif-compote.herokuapp.com'
+  config.action_mailer.smtp_settings = {
+    port:           ENV["MAILGUN_SMTP_PORT"],
+    address:        ENV["MAILGUN_SMTP_SERVER"],
+    user_name:      ENV["MAILGUN_SMTP_LOGIN"],
+    password:       ENV["MAILGUN_SMTP_PASSWORD"],
+    domain:         "iiif-compote.herokuapp.com",
+    authentication: :plain,
+  }
 end
